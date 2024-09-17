2285c2aa37d7785e89cc5ae1fffecee4d9bbd272
MehulMTyagi <mehultyagi10@gmail.com>
2024-09-17 22:53:38 +0530

    py

---
+++ interest calculator.py
@@ -0,0 +1,5 @@
+p=float(input("enter principal: "))
+r=float(input("enter rate of interest: "))
+t=float(input("enter time of interest in years: "))
+SI=p*r*t/100
+print("Your simple interest is:-", SI)
---
+++ name roll no. etc.py
@@ -0,0 +1,3 @@
+name=input("can you tell me your name?  ")
+roll_no=input("enrollment no? ")
+marks=input("and what about marks  ")
